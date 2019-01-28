62274aaa30eb29e3a7e3af1d55adcf5ab9ecf47b
cesar <cesar@cesars-MacBook-Pro.local>
2019-01-27 20:24:47 -0500

    commit one
    commit one

---
+++ configMensaje.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+module.exports = (formulario) => {
+ var transporter = nodemailer.createTransport({
+ service: 'gmail',
+ auth: {
+ user: ' ', // Cambialo por tu email
+ pass: ' ' // Cambialo por tu password
+ }
+ });
+const mailOptions = {
+ from: `”${formulario.nombre} 👻” <${formulario.email}>`,
+ to: 'destinatario', // Cambia esta parte por el destinatario
+ subject: formulario.asunto,
+ html: `
+ <strong>Nombre:</strong> ${formulario.nombre} <br/>
+ <strong>E-mail:</strong> ${formulario.email} <br/>
+ <strong>Mensaje:</strong> ${formulario.mensaje}
+ `
+ };
+transporter.sendMail(mailOptions, function (err, info) {
+ if (err)
+ console.log(err)
+ else
+ console.log(info);
+ });
+}
